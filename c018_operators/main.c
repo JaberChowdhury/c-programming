d33919ba34fcb6fa04e0551c2d99b8d05caf3b7f
black <jaberhc2002@gmail.com>
2024-10-10 21:17:51 +0600

    add : README.md and main.c

--- c018_operators/main.c
+++ c018_operators/main.c
@@ -2,8 +2,24 @@
 
 int main()
 {
-  int a = 5, b= 8, c = 2;
-  int result = a > b ? a : (b % c == 0 ? b : c);
-  printf("%d", result);
+  // Arithmetic operator
+  const int x = 1090;
+  const int y = 8277;
+  int z = 86;
+
+  printf("Addition %d\n", x + y);
+  printf("Subtraction %d\n", x - y);
+  printf("Multiplication %d\n", x * y);
+  printf("Divition %d\n", x / y);
+  printf("Modulas %d\n", x % y);
+  printf("Increment %d\n", ++z);
+  printf("Decrement %d\n", --z);
+
+  
+  // Assignment operator
+
+
+
+
   return 0;
 }
