ef144532461f642bf5747b476edc5a3b314645dd
black <jaberhc2002@gmail.com>
2024-10-11 00:28:10 +0600

    add : c020, c021

---
+++ c020_if_else/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+  const int x = 10;
+  if(x > 11){
+    printf("X is greater than 11");
+  }else if(x == 11){
+    printf("X is equal to 11");
+  }else{
+    printf("X is less than 11");
+  }
+
+  return 0;
+}
---
+++ c021_ternary_operator/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+  const int x = 250;
+  const int y = 22;
+  
+  (x < 30) ? printf("X is less than 30\n") : printf("X is not less than 30\n");
+  
+  (x < y) ?
+    printf("X is less than y %d\n", y - x) :
+  (x > y) ?
+    printf("X is greater than y %d\n", x - y): 
+  printf("X is equal to y");
+
+  return 0;
+}
