69344c3fb0b03bf0211a698d61c2b9db19f29154
black <jaberhc2002@gmail.com>
2024-10-19 00:20:19 +0600

    format : using clang-format -i c-programming/*/*.c

--- c034_pointers/main.c
+++ c034_pointers/main.c
@@ -2,8 +2,8 @@
 
 int main() {
   int number = 1089;
-  int* pointer_number = &number;
-  
+  int *pointer_number = &number;
+
   printf("%d\n", number);
   printf("%p\n", pointer_number);
   printf("%p\n", &number);
--- c034_pointers_&_array/main.c
+++ c034_pointers_&_array/main.c
@@ -1,20 +1,14 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
   int number_array[21] = {
-    18,86,88,
-    87,56,39,
-    57,88,67,
-    78,22,10,
-    19,67,88,
-    87,56,29,
-    57,88,67,
+      18, 86, 88, 87, 56, 39, 57, 88, 67, 78, 22,
+      10, 19, 67, 88, 87, 56, 29, 57, 88, 67,
   };
-  
-  for(int x = 0; x < 21; x++){
+
+  for (int x = 0; x < 21; x++) {
     printf("%p\n", &number_array[x]);
   }
-    printf("%lu\n", sizeof(number_array));
+  printf("%lu\n", sizeof(number_array));
   return 0;
 }
